30424fde1f0ef28de8b5b32e41a2ad60758d4a98
Gold-Sea <1800013512@pku.edu.cn>
2022-08-17 15:49:31 +0800

    com

---
+++ eval/comp.cu
@@ -0,0 +1,414 @@
+/**
+ * Copyright (c) zili zhang & fangyue liu @PKU.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <random>
+#include <cassert>
+#include <cstring>
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <sys/time.h>
+#include <omp.h>
+#include <cinttypes>
+#include <stdint.h>
+#include <algorithm>
+#include <mutex>
+#include <string.h>
+#include <limits>
+#include <memory>
+
+#include <omp.h>
+
+#include <faiss/pipe/IndexIVFPipe.h>
+#include <faiss/gpu/StandardGpuResources.h>
+#include <faiss/gpu/PipeGpuResources.h>
+#include <faiss/gpu/utils/PipeTensor.cuh>
+#include <faiss/pipe/PipeScheduler.h>
+#include <faiss/impl/FaissAssert.h>
+#include <faiss/pipe/PipeKernel.cuh>
+
+#include <faiss/gpu/GpuIndexFlat.h>
+#include <faiss/gpu/GpuIndexIVFFlat.h>
+#include <faiss/gpu/StandardGpuResources.h>
+
+double elapsed() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec * 1e-6;
+}
+
+bool file_exist(const std::string& file_path)
+{
+	if (FILE* file = fopen(file_path.c_str(), "r")){
+		fclose(file);
+		return true;
+	}
+	else 
+		return false;
+}
+
+float* fvecs_read(const char* fname, size_t* d_out, size_t* n_out) {
+    FILE* f = fopen(fname, "r");
+    if (!f) {
+        fprintf(stderr, "could not open %s\n", fname);
+        perror("");
+        abort();
+    }
+    int d;
+    fread(&d, 1, sizeof(int), f);
+    assert((d > 0 && d < 1000000) || !"unreasonable dimension");
+    fseek(f, 0, SEEK_SET);
+    struct stat st;
+    fstat(fileno(f), &st);
+    size_t sz = st.st_size;
+    assert(sz % ((d + 1) * 4) == 0 || !"weird file size");
+    size_t n = sz / ((d + 1) * 4);
+
+    *d_out = d;
+    *n_out = n;
+    float* x = new float[n * (d + 1)];
+    size_t nr = fread(x, sizeof(float), n * (d + 1), f);
+    assert(nr == n * (d + 1) || !"could not read whole file");
+
+    // shift array to remove row headers
+    for (size_t i = 0; i < n; i++)
+        memmove(x + i * d, x + 1 + i * (d + 1), d * sizeof(*x));
+
+    fclose(f);
+    return x;
+}
+
+// not very clean, but works as long as sizeof(int) == sizeof(float)
+int* ivecs_read(const char* fname, size_t* d_out, size_t* n_out) {
+    return (int*)fvecs_read(fname, d_out, n_out);
+}
+
+double inter_sec(int *taget, int *gt, int k){
+    double res = 0.;
+    for (int i = 0; i < k; i++){
+        int val = taget[i];
+        for (int j = 0; j < k; j++){
+            if (val == gt[j]){
+                res += 1.;
+                break;
+            }
+        }
+    }
+    return res / k;
+}
+
+double inter_sec(faiss::Index::idx_t *taget, int *gt, int k){
+    double res = 0.;
+    for (int i = 0; i < k; i++){
+        int val = taget[i];
+        for (int j = 0; j < k; j++){
+            if (val == gt[j]){
+                res += 1.;
+                break;
+            }
+        }
+    }
+    return res / k;
+}
+
+// ./comp 256 10 0.125 0 (bs, topk, nprobe/ncentroids, is our sys or faiss)
+int main(int argc,char **argv){
+    std::cout << argc << " arguments" <<std::endl;
+    if(argc - 1 != 4){
+        printf("You should at least input 3 params: the dataset name, batch size and topk\n");
+        return 0;
+    }
+    // fix the dataset sift
+    std::string db = "/workspace/data/sift/sift10M/sift10M.fvecs";
+    std::string train_db = "/workspace/data/sift/sift1M.fvecs";
+    std::string query = "/workspace/data/sift/sift10M/query.fvecs";
+    std::string gtI = "/workspace/data/sift/sift10M/idx.ivecs";
+    std::string gtD = "/workspace/data/sift/sift10M/dis.fvecs";
+    int dim = 128;
+
+    std::string p1 = argv[1];
+    std::string p2 = argv[2];
+    std::string p3 = argv[3];
+    std::string p4 = argv[4];
+
+    int input_k = std::stoi(p2);
+    int bs = std::stoi(p1);
+    float ratio = std::stof(p3);
+    int che = std::stoi(p4);
+
+    if (che == 0){
+        auto t0 = elapsed();
+
+        omp_set_num_threads(8);
+
+        int ncentroids = 64 * 4;
+        faiss::gpu::PipeGpuResources* pipe_res = new faiss::gpu::PipeGpuResources();
+        faiss::IndexIVFPipeConfig config;
+        faiss::IndexIVFPipe* index;
+
+        index = new faiss::IndexIVFPipe(dim, ncentroids, config, pipe_res, faiss::METRIC_L2);
+        FAISS_ASSERT (config.interleavedLayout == true);
+
+        size_t d;
+        // Train the index
+        {
+            printf("[%.3f s] Loading train set\n", elapsed() - t0);
+
+            size_t nt;
+            float* xt = fvecs_read(train_db.c_str(), &d, &nt);
+
+            FAISS_ASSERT(d == dim);
+            printf("[%.3f s] Training on %ld vectors\n", elapsed() - t0, nt);
+
+            index->train(nt, xt);
+            delete[] xt;
+        }
+        // Add the data
+        {
+            printf("[%.3f s] Loading database\n", elapsed() - t0);
+
+            size_t nb, d2;
+            auto xb = fvecs_read(db.c_str(), &d2, &nb);
+            assert(d == d2 || !"dataset does not have same dimension as train set");
+
+            printf("[%.3f s] Indexing database, size %ld*%ld\n",
+                elapsed() - t0,
+                nb,
+                d);
+            
+            index->add(nb, xb);
+
+            delete[] xb;
+        }
+
+        size_t nq;
+        float* xq;
+        {
+            printf("[%.3f s] Loading queries\n", elapsed() - t0);
+
+            size_t d2;
+            xq = fvecs_read(query.c_str(), &d2, &nq);
+            assert(d == d2 || !"query does not have same dimension as train set");
+        }
+
+        size_t k;                // nb of results per query in the GT
+        int* gt; // nq * k matrix of ground-truth nearest-neighbors
+
+        {
+            printf("[%.3f s] Loading ground truth for %ld queries\n",
+                elapsed() - t0,
+                nq);
+
+            // load ground-truth and convert int to long
+            size_t nq2;
+            int* gt_int = ivecs_read(gtI.c_str(), &k, &nq2);
+            assert(nq2 == nq || !"incorrect nb of ground truth entries");
+
+            gt = new int[k * nq];
+            for (int i = 0; i < k * nq; i++) {
+                gt[i] = gt_int[i];
+            }
+            delete[] gt_int;
+        }
+
+        float *gtd;
+        {
+            printf("[%.3f s] Loading ground truth for %ld queries\n",
+                elapsed() - t0,
+                nq);
+
+            // load ground-truth and convert int to long
+            size_t nq2;
+            gtd = fvecs_read(gtD.c_str(), &k, &nq2);
+            assert(nq2 == nq || !"incorrect nb of ground truth entries");
+        }
+        printf("[%.3f s] Start Balancing\n",
+               elapsed() - t0);
+        index->balance();
+        printf("[%.3f s] Finishing Balancing: %d B clusters\n",
+                elapsed() - t0, index->pipe_cluster->bnlist);
+
+        auto pc = index->pipe_cluster;
+        pipe_res->initializeForDevice(0, pc);
+
+        // Profile
+        printf("[%.3f s] Start Profile\n",
+                elapsed() - t0);
+        // Train profile
+        std::string profile_name = "Profile_" + p1 + "10M_" + std::to_string(ncentroids) + ".txt";
+        if (!file_exist(profile_name.c_str())){
+            index->profile();
+            index->saveProfile(profile_name.c_str());
+        }
+        else{
+            index->loadProfile(profile_name.c_str());
+        }
+        printf("[%.3f s] Finish Profile\n",
+                elapsed() - t0);
+        nq = 2560;
+        // Start queries
+        std::vector<float> dis(nq * input_k);
+        std::vector<int> idx(nq * input_k);
+        // Add the dataset
+        {
+            index->set_nprobe(ncentroids);
+            auto sche = new faiss::gpu::PipeScheduler(index, 
+                pc, pipe_res, 1, xq, input_k, dis.data(), idx.data());
+        }
+        index->set_nprobe(int(ncentroids * ratio));
+        double tt0, tt1, total = 0., opt = 0.;
+
+        int i;
+        for (i = 0; i < nq / bs; i++){
+            tt0 = elapsed();
+            auto sche = new faiss::gpu::PipeScheduler(index, 
+                pc, pipe_res, bs, xq + d * (bs * i), input_k, dis.data() + input_k * (bs * i), idx.data() + input_k * (bs * i));
+            tt1 = elapsed();
+            printf("Computation Time: %.3f ms, Transmission Time: %.3f ms\n", 
+                sche->com_time*1000, sche->com_transmission*1000);
+            total += sche->com_time*1000;
+            opt += std::max(sche->com_time*1000, sche->com_transmission*1000);
+            delete sche;
+        }
+
+        double acc = 0.;
+        for (int j = 0; j < i * bs; j++){
+            acc += inter_sec(idx.data() + input_k * j, gt + k * j, input_k);
+        }
+
+        printf("Ave Latency : %.3f ms\n", total / i);
+        printf("Ave accuracy : %.1f%% \n", acc * 100 / (i*bs));
+
+        delete[] xq;
+        delete[] gt;
+        delete[] gtd;
+        delete index;
+        delete pipe_res;
+    }
+    else if (che == 1){
+        auto t0 = elapsed();
+        omp_set_num_threads(8);
+        int ncentroids = 256 * 8;
+
+        int dev_no = 0;
+        faiss::gpu::StandardGpuResources resources;
+        faiss::gpu::GpuIndexIVFFlatConfig config;
+        config.device = dev_no;
+        faiss::gpu::GpuIndexIVFFlat index(&resources, dim, ncentroids, faiss::METRIC_L2, config);
+        size_t d;
+        // Train the index
+        {
+            printf("[%.3f s] Loading train set\n", elapsed() - t0);
+
+            size_t nt;
+            float* xt = fvecs_read(train_db.c_str(), &d, &nt);
+
+            FAISS_ASSERT(d == dim);
+            printf("[%.3f s] Training on %ld vectors\n", elapsed() - t0, nt);
+
+            index.train(nt, xt);
+            delete[] xt;
+        }
+
+        // Add the data
+        {
+            printf("[%.3f s] Loading database\n", elapsed() - t0);
+
+            size_t nb, d2;
+            auto xb = fvecs_read(db.c_str(), &d2, &nb);
+            assert(d == d2 || !"dataset does not have same dimension as train set");
+
+            printf("[%.3f s] Indexing database, size %ld*%ld\n",
+                elapsed() - t0,
+                nb,
+                d);
+            
+            index.add(nb, xb);
+
+            delete[] xb;
+        }
+
+        size_t nq;
+        float* xq;
+        {
+            printf("[%.3f s] Loading queries\n", elapsed() - t0);
+
+            size_t d2;
+            xq = fvecs_read(query.c_str(), &d2, &nq);
+            assert(d == d2 || !"query does not have same dimension as train set");
+        }
+
+        size_t k;                // nb of results per query in the GT
+        int* gt; // nq * k matrix of ground-truth nearest-neighbors
+
+        {
+            printf("[%.3f s] Loading ground truth for %ld queries\n",
+                elapsed() - t0,
+                nq);
+
+            // load ground-truth and convert int to long
+            size_t nq2;
+            int* gt_int = ivecs_read(gtI.c_str(), &k, &nq2);
+            assert(nq2 == nq || !"incorrect nb of ground truth entries");
+
+            gt = new int[k * nq];
+            for (int i = 0; i < k * nq; i++) {
+                gt[i] = gt_int[i];
+            }
+            delete[] gt_int;
+        }
+
+        float *gtd;
+        {
+            printf("[%.3f s] Loading ground truth for %ld queries\n",
+                elapsed() - t0,
+                nq);
+
+            // load ground-truth and convert int to long
+            size_t nq2;
+            gtd = fvecs_read(gtD.c_str(), &k, &nq2);
+            assert(nq2 == nq || !"incorrect nb of ground truth entries");
+        }
+        nq = 2560;
+        // Start queries
+        std::vector<float> dis(nq * input_k);
+        std::vector<faiss::Index::idx_t> idx(nq * input_k);
+        index.nprobe = int(ncentroids * ratio);
+
+        auto tt0 = elapsed();
+        int i;
+        for (i = 0; i < nq / bs; i++){
+            index.search(bs, xq + d * (bs * i), input_k, dis.data() + input_k * (bs * i), idx.data() + input_k * (bs * i));
+        }
+        auto tt1 = elapsed();
+        double total = tt1 - tt0;
+
+        double acc = 0.;
+        for (int j = 0; j < i * bs; j++){
+            auto tmp = inter_sec(idx.data() + input_k * j, gt + k * j, input_k);
+            acc += tmp;
+        }
+        acc /= (i * bs);
+        acc *= 100;
+
+        printf("Ave Latency : %.3f ms\n", total / i * 1000);
+        printf("Ave accuracy : %.1f%% \n", acc);
+
+        delete[] xq;
+        delete[] gt;
+        delete[] gtd;
+    }
+    else{
+        assert(false);
+    }
+    return 0;
+}
